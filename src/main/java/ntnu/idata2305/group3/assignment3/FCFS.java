e947374882a628859855821808ee38bcfd04486e
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-03-25 15:49:55 +0100

    updated fcfs, only need to sort by arrival time left

--- src/main/java/ntnu/idata2305/group3/assignment3/FCFS.java
+++ src/main/java/ntnu/idata2305/group3/assignment3/FCFS.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class FCFS {
     public static void main(String args[]) {
 
+        //gets inputs for how many processes is wished for.
         System.out.println("Enter How many processes you want: ");
         Scanner input = new Scanner(System.in);
         int numbersNeeded = input.nextInt();
 
+        //values needed for algo.
         int processID[] = new int[numbersNeeded];
         int arrivalTime[] = new int[numbersNeeded];
         int burstTime[] = new int[numbersNeeded];
-        int waitingTime[] = new int[numbersNeeded];
+        int completionTime[] = new int[numbersNeeded];
         int turnaroundTime[] = new int[numbersNeeded];
+        int waitingTime[] = new int[numbersNeeded];
+        int currentTime = 0;
 
+        //gets inputs for arrival time and burst time.
         for(int i = 0; i < numbersNeeded; i++) {
             System.out.println("Enter Arrival Time for process " + (i+1) + ": ");
             arrivalTime[i] = input.nextInt();
@@ -23,10 +28,21 @@ public class FCFS {
             processID[i] = i+1;
         }
 
+        // calculates completion time, turnaround time and waiting time.
+        for(int i = 0; i < numbersNeeded; i++) {
+            if(currentTime < arrivalTime[i]) {
+                currentTime = arrivalTime[i];
+            }
+            completionTime[i] = currentTime + burstTime[i];
+            turnaroundTime[i] = completionTime[i] - arrivalTime[i];
+            waitingTime[i] = turnaroundTime[i] - burstTime[i];
+            currentTime = completionTime[i];
+        }
 
+        // prints out all relevant values.
         for(int i = 0; i < numbersNeeded; i++) {
             System.out.println("Process ID: " + processID[i] + " Arrival Time: " + arrivalTime[i] + " Burst Time: " + burstTime[i] +
-                    " Waiting Time: " + waitingTime[i] + " Turnaround Time: " + turnaroundTime[i]);
+                    " Completion Time: " + completionTime[i] + " Waiting Time: " + waitingTime[i] + " Turnaround Time: " + turnaroundTime[i]);
         }
     }
 }
