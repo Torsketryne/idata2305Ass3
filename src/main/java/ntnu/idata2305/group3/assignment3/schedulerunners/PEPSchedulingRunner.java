8bb26543d4f65701f02119a2c5fbe038b75656d8
nikoaa <nikoaa@stud.ntnu.no>
2025-03-25 15:10:27 +0100

    Suggestion classes for exploring options

---
+++ src/main/java/ntnu/idata2305/group3/assignment3/schedulerunners/PEPSchedulingRunner.java
@@ -0,0 +1,4 @@
+package ntnu.idata2305.group3.assignment3.schedulerunners;
+
+public class PEPSchedulingRunner {
+}
---
+++ src/main/java/ntnu/idata2305/group3/assignment3/schedulerunners/QueueSchedulingRunner.java
@@ -0,0 +1,7 @@
+package ntnu.idata2305.group3.assignment3.schedulerunners;
+
+public class QueueSchedulingRunner {
+  public static void main(String[] args) {
+
+  }
+}
